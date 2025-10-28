54f8e7256b17eb0b803da18a30d40200de9dc7d5
git-sudo-404 <git-sudo-404@users.noreply.github.com>
2025-10-28 19:18:00 +0530

    simple server

--- server.ts
+++ server.ts
@@ -4,19 +4,21 @@ import "dotenv/config";
 const PORT = process.env.PORT;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "content-type": "text/json" });
-  res.write(
-    JSON.stringify({
-      firstName: "RED",
-      lastName: "Coder",
-      CP: "Grandmaster",
-      job: "SDE at Google",
-      para: "Let's see if this paragrapgh turns into an array"
-        .trim()
-        .split(" "),
-    }),
-  );
-  res.end();
+  if (req.url === "/") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write(`<h1>
+    HomePage
+    </h1>
+    <p>This is the first paragraph</p>
+`);
+    res.end();
+  } else if (req.url === "/about") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<p>This is the about Page</h1>");
+  } else {
+    res.writeHead(404);
+    res.write("<h1>Page not Found!</h1>");
+  }
 });
 
 server.listen(PORT, () => {
