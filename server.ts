b7f2bf0cab634e9d8531b6e6063520438d1ecd8e
git-sudo-404 <git-sudo-404@users.noreply.github.com>
2025-10-28 19:27:14 +0530

    update

--- server.ts
+++ server.ts
@@ -3,24 +3,29 @@ import "dotenv/config";
 
 const PORT = process.env.PORT;
 
-const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.write(`<h1>
-    HomePage
-    </h1>
-    <p>This is the first paragraph</p>
-`);
-    res.end();
-  } else if (req.url === "/about") {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.write("<p>This is the about Page</h1>");
-  } else {
-    res.writeHead(404);
-    res.write("<h1>Page not Found!</h1>");
-  }
-});
+const server = http.createServer(
+  (req: http.IncomingMessage, res: http.ServerResponse) => {
+    if (req.method === "GET") {
+      processGetRequests(req, res);
+    } else if (req.method === "POST") {
+      processPostRequests(req, res);
+    } else {
+      res.writeHead(404, { "content-type": "text/plain" });
+      res.end("WRONG Request Mothod");
+    }
+  },
+);
 
 server.listen(PORT, () => {
   console.log(`Server running on PORT : ${PORT}`);
 });
+
+const processGetRequests = (
+  req: http.IncomingMessage,
+  res: http.ServerResponse,
+) => { };
+
+const processPostRequests = (
+  req: http.IncomingMessage,
+  http: http.ServerResponse,
+) => { };
