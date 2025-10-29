31172f73a0ae9491b9e3a66bbd938ef70a574466
git-sudo-404 <git-sudo-404@users.noreply.github.com>
2025-10-29 16:19:23 +0530

    added types

--- routes/notfound.ts
+++
@@ -1,9 +0,0 @@
-import http from "http";
-import { handleNotFoundPage } from "../lib/utils.ts";
-
-export const handleNotFound = (
-  req: http.IncomingMessage,
-  res: http.ServerResponse,
-) => {
-  handleNotFoundPage(req, res);
-};
--- server.ts
+++ server.ts
@@ -1,25 +1,35 @@
+import express from "express";
+import { Request, Response } from "express";
 import http from "http";
 import "dotenv/config";
-import { handleNotFound } from "./routes/notfound.ts";
-import { handleHomeRoute } from "./routes/homepage.ts";
-import { handleAboutRoute } from "./routes/aboutpage.ts";
-import url, { fileURLToPath } from "url";
-import path from "path";
 
-const PORT = process.env.PORT;
+const app = express();
 
-const server = http.createServer(
-  (req: http.IncomingMessage, res: http.ServerResponse) => {
-    if (req.url === "/") {
-      handleHomeRoute(req, res);
-    } else if (req.url === "/about") {
-      handleAboutRoute(req, res);
-    } else {
-      handleNotFound(req, res);
-    }
-  },
-);
+//NOTE: use this line so as to parse the incoming json streams into req.body
+app.use(express.json());
 
-server.listen(PORT, () => {
-  console.log("SERVER LISTENING ON PORT : ", PORT);
+app.listen(process.env.PORT, () => {
+  console.log("Server running on PORT : ", process.env.PORT);
+});
+
+//NOTE: This just sends the status alone , even the json is being sent in code , it just sends only the status code and returns.
+// app.get("/", (req, res) => {
+//   res.sendStatus(200);
+//   res.send({
+//     name: "redCoder",
+//   });
+// });
+
+//FIX: You can't use http.IncomingMessage & http.ServerResponse from "http" for the (req,res) of express , instead use Request & Response from express.
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).send({ name: "redcoder" });
+});
+
+app.post("/", (req: Request, res: Response) => {
+  console.log("GOT NEW REQUEST  :");
+  console.log(req.body);
+  res.status(200).send({
+    message: "Request recieved successfuly",
+  });
 });
